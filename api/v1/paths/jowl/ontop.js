1a45ea117a0691a4b9d4df599d57ab9a61e987c0
Joao Pedro Jensen Ourique <joao.ourique@aprix.com.br>
2024-02-27 08:51:56 -0300

    feat: created ontop api functions for each endpoint

---
+++ api/v1/paths/jowl/ontop.js
@@ -0,0 +1,69 @@
+const { processResponse } = require("../utils");
+const ConfigManager = require("../../../../bin/configManager.");
+const httpProxy = require("../../../../bin/httpProxy.");
+const {INIT_REPO_BODY, INIT_REPO_URL, QUERY_BODY, QUERY_URL} = require("./ontopParams/ontopParams");
+
+module.exports = function () {
+    let operations = {
+        POST,
+    };
+
+    async function POST(req, res, next) {
+        const allowedOperationIds = ["init_repo", "query"];
+        let operationId = req.body.operationId;
+        if (!allowedOperationIds.includes(operationId)) {
+            return res.status(400).json({ error: "missing valid operationId. Must be either initRepo or query" });
+        }
+        var jowlConfig = ConfigManager.config.jowlServer;
+
+        if (operationId === "init_repo") {
+            const url = jowlConfig.url + INIT_REPO_URL;
+            httpProxy.post(url, INIT_REPO_BODY, function (err, result) {
+                if (err) {
+                    next(err);
+                } else {
+                    return processResponse(res, err, JSON.parse(result));
+                }
+            });
+        }
+        if (operationId === "query") {
+            const url = jowlConfig.url + QUERY_URL;
+            httpProxy.post(url, QUERY_BODY, function (err, result) {
+                if (err) {
+                    next(err);
+                } else {
+                    return processResponse(res, err, JSON.parse(result));
+                }
+            });
+        }
+    }
+
+    POST.apiDoc = {
+        summary: "Ontop API",
+        security: [{ restrictLoggedUser: [] }],
+        parameters: [
+            {
+                name: "body",
+                description: "body",
+                in: "body",
+                schema: {
+                    type: "object",
+                    properties: {
+                        operationId: {
+                            type: "string",
+                            enum: ["init_repo", "query"],
+                        },
+                    },
+                },
+            },
+        ],
+        responses: {
+            200: {
+                description: "Response",
+                schema: {},
+            },
+        },
+    };
+
+    return operations;
+};
